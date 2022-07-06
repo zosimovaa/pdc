ec5ccdd5f1e02e65171d28b9c7e40a7419135cbc
Алексей Зосимов <lesha.spb@gmail.com>
2022-07-06 23:35:52 +0300

    Version update

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 class PdcLiteApp(BasicApplication):
     NAME = "Poloniex Data Collector"
-    VERSION = 1.2
+    VERSION = 1.3
     MAX_TIMEOUT = 180
 
     def __init__(self, config):
